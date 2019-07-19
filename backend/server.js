fcd5ef61e115d5beaab7e11123e2fd77596ac42c
Jiachen Tu <mikejiachen@outlook.com>
2019-07-18 23:20:57 -0400

    Mongoose connection added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const dbRoutes = require('./routes/databaseAccess.js');
 
+const mongoose = require('mongoose');
+//const bodyParser = require('body-parser');
+var connect = process.env.MONGODB_URI;
+mongoose.connect(connect);
+
 // This line makes the build folder publicly available.
 app.use(express.static('build'));
 app.use('/db', dbRoutes);
